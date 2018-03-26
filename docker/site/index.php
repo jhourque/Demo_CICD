ddf6817dcef2e9f2c5088eff5d6966ab48b5cb9c
Jérome HOURQUEBIE <jerome.hourquebie@d2-si.eu>
2018-03-26 15:05:46 +0000

    change on dev branch

--- docker/site/index.php
+++ docker/site/index.php
@@ -101,7 +101,7 @@ array_multisort($last,SORT_ASC, SORT_STRING,$first)
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#page-top">#TIAD</a>
+                <a class="navbar-brand" href="#page-top">#TIAD Paris 2018</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->

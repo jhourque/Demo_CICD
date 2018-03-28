8c248b2aeb1a3a60888a15731b39e3833589f0b5
Jérome HOURQUEBIE <jerome.hourquebie@d2-si.eu>
2018-03-28 10:11:18 +0000

    fix

--- Jenkins/scripts/set-config.sh
+++ Jenkins/scripts/set-config.sh
@@ -18,7 +18,12 @@ SSH_PUB=$(cat ~/.ssh/id_rsa.jenkins.pub)
 cat admin.groovy.skel | sed "s#SSH_PUB#$SSH_PUB#" > admin.groovy
 
 # Upload script on Jenkins VM
-scp -i ~/.ssh/id_rsa.jenkins admin.groovy jenkins-cli.jar ubuntu@$JENKINS_IP:
+scp -i ~/.ssh/id_rsa.jenkins admin.groovy jenkins-cli.jar ~/.ssh/id_rsa.demo ubuntu@$JENKINS_IP:
+
+# upload id_rsa.demo key to /var/lib/jenkins/.ssh
+ssh -i ~/.ssh/id_rsa.jenkins ubuntu@$JENKINS_IP "sudo mkdir /var/lib/jenkins/.ssh"
+ssh -i ~/.ssh/id_rsa.jenkins ubuntu@$JENKINS_IP "sudo mv /home/ubuntu/id_rsa.demo /var/lib/jenkins/.ssh"
+ssh -i ~/.ssh/id_rsa.jenkins ubuntu@$JENKINS_IP "sudo chown -R jenkins:jenkins /var/lib/jenkins/.ssh"
 
 # Run groovy script (create admin account & set security)
 ssh -i ~/.ssh/id_rsa.jenkins ubuntu@$JENKINS_IP "cat admin.groovy  | java -jar jenkins-cli.jar -s $URL groovy ="
@@ -29,3 +34,5 @@ cat XML/aws_creds.xml  |sed "s/AWS_ACCESS_KEY_ID/$AWS_ACCESS_KEY_ID/" |sed "s AW
 # Create Dev & Prod Pipelines
 cat XML/Dev_pipeline.xml |java -jar jenkins-cli.jar -s $JENKINS_URL -ssh -user admin -i ~/.ssh/id_rsa.jenkins -p 50022 create-job Dev_pipeline
 cat XML/Prod_pipeline.xml |java -jar jenkins-cli.jar -s $JENKINS_URL -ssh -user admin -i ~/.ssh/id_rsa.jenkins -p 50022 create-job Prod_pipeline
+
+
